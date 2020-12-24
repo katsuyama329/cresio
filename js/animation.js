2f5ec98ceeb6f44c52666c590007679f3a20a119
katsuyama <psisnkitke@gmail.com>
2020-12-24 18:51:08 +0900

    pc画面修正&js追加&mainvisual修正

---
+++ js/animation.js
@@ -0,0 +1,18 @@
+$(function(){
+    $(window).scroll(function (){
+        $(".u-animation").each(function(){
+            var imgPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + windowHeight/5){
+                $(this).addClass("fade_on");
+            } 
+            else {
+                $(this).removeClass("fade_on");
+            }
+        });
+    });
+});
+
+
+
